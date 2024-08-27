63c24d6b75c52b4298447bf4fcb052a02a759951
VITOR PAES KOLLE <23111830@dsn127m1241955.cfp127aluno.local>
2024-08-27 09:48:08 -0300

    config retrofit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,8 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    //Retrofit Dependencies
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
 }
